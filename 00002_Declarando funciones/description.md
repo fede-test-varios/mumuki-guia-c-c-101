b9fc1c9b97344410f9676a05d78e5d9883000c71
fedescarpa@mumuki.org <fedescarpa@mumuki.org>
2020-01-06 23:01:22 +0000

    Mumuki Export on 2020-01-06 23:01:22 +0000

---
+++ 00002_Declarando funciones/description.md
@@ -0,0 +1,3 @@
+Hola, declará la función `sumar`, 
+
+> Esta funcióñ toma 2 números y te retorna el resultado de la suma entre ambos.
---
+++ 00002_Declarando funciones/test.c
@@ -0,0 +1,5 @@
+describe ("Group description") {
+  it ("Sample description") {
+    should_int(sumar(2, 3)) be equal to(5);
+  } end
+} end
