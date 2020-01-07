80eda325cbb0c164942ee740628f450553e3fcb4
juan.mesaglio@gmail.com <juan.mesaglio@gmail.com>
2020-01-07 17:26:39 +0000

    Mumuki Export on 2020-01-07 17:26:39 +0000

--- 00001_Declarando Variables/test.c
+++ 00001_Declarando Variables/test.c
@@ -1,5 +1,11 @@
 describe ("Chequeo de tipos") {
-  it ("cantidadDeAlumnos Int") {
-    should_string(typenasme(cantidadDeAlumnos)) be equal to("int");
+  it ("cantidadDeAlumnos int") {
+    should_int(cantidadDeAlumnos) be equal to(20);
+  } end
+  it ("saldoBancario float") {
+    should_floar(saldoBanco) be equal to(20000);
+  } end
+  it ("inicial de nombre") {
+    should_char(inicial) be equal to('J');
   } end
 } end
