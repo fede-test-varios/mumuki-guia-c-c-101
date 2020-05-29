c15ad70428de3de1dc1ff21c729c46cfd39d40f4
juan.mesaglio@gmail.com <juan.mesaglio@gmail.com>
2020-05-29 21:03:02 +0000

    Mumuki Export on 2020-05-29 21:03:02 +0000

--- 00006_Estrcturas v3/test.c
+++ 00006_Estrcturas v3/test.c
@@ -1,6 +1,17 @@
-describe ("Personas con auto") {
+context{
+  t_auto* corsa = malloc(sizeof(t_auto));
+  corsa->marca = "Chevrolet";
+  corsa->modelo = "Crosa Classic";
+  corsa->patente = "NBV678";
+  corsa->anio = 2006;
+  t_persona* facundo = malloc(sizeof(t_persona));
+  facundo->nombre="Facundo";
+  facundo->apellido="Gonzalez";
+  facundo->fechaDeNacimiento="10/01/1996";
+  describe ("Personas con auto") {
   
-  it ("Descripción del ejemplo") {
-    should_bool(true) be equal to(true);
+    it ("Descripción del ejemplo") {
+      should_bool(true) be equal to(true);
+    } end
   } end
-} end
+}
