089b1e24cd1c098db759744bf2232dfac03c4400
juan.mesaglio@gmail.com <juan.mesaglio@gmail.com>
2020-05-29 20:05:48 +0000

    Mumuki Export on 2020-05-29 20:05:48 +0000

--- 00004_Estructuras v1/test.c
+++ 00004_Estructuras v1/test.c
@@ -1,14 +1,14 @@
 describe ("Persona") {
     it ("El nombre debe ser Facundo") {
-    should_string(persona->nombre) be equal to("Facundo");
+    should_string(persona.nombre) be equal to("Facundo");
   } end
   it ("El apellido debe ser Gonzalez") {
-    should_string(persona->apellido) be equal to("Gonzalez");
+    should_string(persona.apellido) be equal to("Gonzalez");
   } end
   it ("La edad debe ser 24") {
-    should_int(persona->edad) be equal to(24);
+    should_int(persona.edad) be equal to(24);
   } end
   it ("La fecha de nacimiento debe ser 10/01/1996") {
-    should_string(persona->fechaDeNacimiento) be equal to("10/01/1996");
+    should_string(persona.fechaDeNacimiento) be equal to("10/01/1996");
   } end
 } end
