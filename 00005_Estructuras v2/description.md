f1d709c58cbead60eca82deb7ec9a6adf887db45
juan.mesaglio@gmail.com <juan.mesaglio@gmail.com>
2020-05-29 20:25:17 +0000

    Mumuki Export on 2020-05-29 20:25:17 +0000

--- 00005_Estructuras v2/description.md
+++ 00005_Estructuras v2/description.md
@@ -3,4 +3,16 @@ Porque?? Lo veremos mas adelante.
 
 Al utilizar un puntero, reemplazamos el operador `.` por `->`.
 
-Se usa muy parecido, `punteroALaEstructura.campoDeEstructura = valor`
+Se usa muy parecido, `punteroALaEstructura->campoDeEstructura = valor`.
+
+Ahora tenemos autos
+```c
+typedef struct {
+    char* marca;
+    char* modelo;
+    char* patente;
+    int anio;
+} t_auto;
+```
+
+Crear una variable `corsa`,que sea un puntero a t_auto. Obviamente su marca tiene que ser Chevrolet, modelo Crosa Classic, con patente NBV678, del 2006. 
--- 00005_Estructuras v2/test.c
+++ 00005_Estructuras v2/test.c
@@ -1,5 +1,14 @@
-describe ("Descripción del grupo") {
-  it ("Descripción del ejemplo") {
-    should_bool(true) be equal to(true);
-  } end
+describe ("*Auto") {
+    it ("Marca del auto") {
+        should_string(corsa->marca) be equal to("Chevrolet");
+    } end
+    it ("Modelo del auto") {
+        should_string(corsa->modelo) be equal to("Crosa Classic");
+    } end
+    it ("Año del auto") {
+        should_int(corsa->anio) be equal to(2006);
+    } end
+    it ("Patente del auto") {
+        should_string(corsa->patente) be equal to("NBV678");
+    } end
 } end
