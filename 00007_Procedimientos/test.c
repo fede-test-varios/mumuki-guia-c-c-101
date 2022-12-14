b36ea6bfe8b509896ab0a0e31f3d09774ade5768
fedescarpa@mumuki.org <fedescarpa@mumuki.org>
2022-12-14 19:39:16 +0000

    Mumuki Export on 2022-12-14 19:39:16 +0000

--- 00007_Procedimientos/test.c
+++ 00007_Procedimientos/test.c
@@ -1,6 +1,20 @@
 int cantidadDeDiasCorridosSinAccidentes = 0;
-describe ("Descripción del grupo") {
-  it ("Descripción del ejemplo") {
-    should_bool(true) be equal to(true);
+
+describe ("Procedimientos") {
+  
+  before {
+    cantidadDeDiasCorridosSinAccidentes = 10;
   } end
+  
+  it ("dia_bueno") {
+    dia_bueno();
+    dia_bueno();
+    should_int(cantidadDeDiasCorridosSinAccidentes) be equal to(12);
+  } end
+  
+  it ("dia_malo") {
+    dia_malo();
+    should_int(cantidadDeDiasCorridosSinAccidentes) be equal to(0);
+  } end
+  
 } end
